54f1ebd0b34633c14bbdf1e1c8aef3f78a6306d2
Matias Arenas <matias@groupon.com>
2015-11-02 22:35:37 -0800

    Initial Commit

---
+++ Mattwitter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Mattwitter
+//
+//  Created by Matias Arenas Sepulveda on 11/2/15.
+//  Copyright © 2015 Matias Arenas Sepulveda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
